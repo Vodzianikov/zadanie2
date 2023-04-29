80ccf1d36c5fbfe7a9f75bd3535ceb2a439f83b9
Vodzianikov <125147632+Vodzianikov@users.noreply.github.com>
2023-04-29 23:12:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let age = prompt('число от 1 до 7');
+
+
+switch (age) {
+    case 1:
+        console.log('Понедельник');
+        break;
+
+    case 2:
+        console.log('Вторник');
+        break;
+
+    case 3:
+        console.log('Среда');
+        break;
+
+    case 4:
+        console.log('Четверг');
+        break;
+
+    case 5:
+        console.log('Пятница');
+        break;
+
+    case 6:
+        console.log('Выходной');
+        break;
+
+    case 7:
+        console.log('Выходной');
+        break;
+        
+    default:
+        console.log('error');
+        break;
+}
